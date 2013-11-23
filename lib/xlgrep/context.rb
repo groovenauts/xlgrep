553a78121295248bc819d7f8d5448f399ea2c7de
akima <akm2000@gmail.com>
2013-11-23 18:16:34 +0900

    ターミナルの標準出力へ出力する場合には処理中のファイルやシート名を表示するようにしました。

--- lib/xlgrep/context.rb
+++ lib/xlgrep/context.rb
@@ -1,6 +1,7 @@
 require 'xlgrep'
 
 require 'roo'
+require 'highline'
 
 module Xlgrep
   class Context
@@ -17,10 +18,10 @@ module Xlgrep
 
     def execute(files)
       files.each do |f|
-        $stdout.puts("loading #{f}")
         book = book_for(f)
         book.sheets.each do |sheet|
-          $stdout.puts("\rloading #{f} #{sheet}")
+          print_status("loading #{f} #{sheet}")
+
           book.default_sheet = sheet
           (book.first_row..book.last_row).each do |r|
             cells = book.row(r)
@@ -29,20 +30,32 @@ module Xlgrep
                 pred.match(cell) do |msg|
                   a, b = idx.divmod(26) # ('A'..'Z').length => 26
                   x = (a > 0 ? (BASE_CHAR_ORDER + a).chr : "") + (BASE_CHAR_ORDER + b).chr
+                  print_status("")
                   @formatter.process({
                     file: f, sheet: sheet,
                     x: x, y: r,
                     data: cell,
                     msg: msg
                   })
+                  print_status("loading #{f} #{sheet}")
                 end
               end
             end
           end
         end
-        $stdout.puts(" loaded #{f}")
+        # print_status(" loaded #{f}")
+        # puts
       end
+      print_status("")
       self
+    end
+
+    def print_status(s)
+      return unless $stdout.tty?
+      cols, rows = HighLine::SystemExtensions.terminal_size # [columns, lines]
+      slen = cols - s.bytesize
+      slen = 0 if slen < 0
+      $stdout.print("\r" + s + (" " * slen))
     end
 
   end
